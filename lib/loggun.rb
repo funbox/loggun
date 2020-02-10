cea54c768d31e7b352ec87b777d4ce69547c5208
Aleksandr Noskov <nemestny@politeh.ru>
2020-02-10 14:25:54 +0500

    Generate gem

---
+++ lib/loggun.rb
@@ -0,0 +1,6 @@
+require 'loggun/version'
+
+module Loggun
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/loggun_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Loggun do
+  it 'has a version number' do
+    expect(Loggun::VERSION).not_to be nil
+  end
+
+  it 'does something useful' do
+    expect(false).to eq(true)
+  end
+end
